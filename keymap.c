bc7ec2db49b3f44819b01f3f974d3d9904262dcd
mxp <maxplayer@free.fr>
2022-11-11 22:13:20 +0100

    issue with windows on keypad equal

--- keymap.c
+++ keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10, KC_PEQL,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10, KC_EQL ,
     _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, QUOTE,
     _______, _______, _______, TXT_COPY,TXT_PSTE,_______, KC_F11,    KC_F12,  _______, _______, _______, _______, _______, KC_PIPE,
