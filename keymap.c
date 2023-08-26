a0a488d67bee752ed302cf30a05358ad3fde89cd
mxp <maxplayer@free.fr>
2023-08-26 23:13:42 +0200

    ajout de l'euros et des touches echape et delete en tap sur les touche de layer bascule de quelques keymaps pour faire matcher redox et lily

--- keymap.c
+++ keymap.c
@@ -11,7 +11,7 @@
 #define _LOWER 1
 #define _RAISE 2
 
-enum custom_keycodes { 
+enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
@@ -19,7 +19,8 @@ enum custom_keycodes {
   TXT_PSTE,
   CEDILLE,
   QUOTE,
-  DBQUOTE
+  DBQUOTE,
+  EURO
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -53,6 +54,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
          SEND_STRING(SS_LSFT("' "));
       }
       break;
+
+   case EURO:
+      if (record->event.pressed) {
+         SEND_STRING(SS_RALT("5"));
+      }
+      break;
    }
    return true;
 };
@@ -64,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
     KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_MINS, KC_SLSH,
-                              KC_LGUI,KC_LALT,MO(_LOWER), KC_BSPC,  KC_SPC,   MO(_RAISE),KC_INS, KC_PSCR
+                              KC_LGUI,KC_LALT,LT(_LOWER,KC_ESC),KC_BSPC,KC_SPC,LT(_RAISE,KC_DEL),KC_PSCR,KC_INS
   ),
 
   [_LOWER] = LAYOUT(
@@ -77,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10, KC_PPLS,
-    _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, EURO   , _______, _______,                     _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT, _______, DBQUOTE,
     _______, _______, _______, CEDILLE, _______, _______, KC_F11,    KC_F12,  _______, _______, _______, _______, _______, KC_BSLS,
                                _______, _______, _______, KC_DEL,    _______, _______, _______, _______
