6aa28fe1c2fafe757359cafc2aeb054757483032
jmnguye <nguyen.jean.michel@gmail.com>
2024-10-15 21:15:39 +0200

    adding tap dance, changing layout

--- keymap.c
+++ keymap.c
@@ -4,6 +4,10 @@
 #define _LOWER 1
 #define _RAISE 2
 
+enum {
+    TD_LALT_ESC
+};
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -14,6 +18,10 @@ enum custom_keycodes {
   QUOTE,
   DBQUOTE,
   EURO
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_LALT_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_ESC)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -64,23 +72,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
     KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_MINS, KC_SLSH,
-                              KC_LGUI,KC_LALT,LT(_LOWER,KC_ESC),KC_BSPC,KC_SPC,LT(_RAISE,KC_DEL),KC_PSCR,KC_INS
+                         KC_PSCR,KC_LGUI,TD(TD_LALT_ESC),LT(_LOWER,KC_BSPC),LT(_RAISE,KC_SPC),KC_ALGR,KC_INS,KC_ALGR
   ),
 
   [_LOWER] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10, KC_EQL ,
     _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,                     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, QUOTE  ,
-    _______, _______, _______, _______,TXT_COPY,TXT_PSTE, KC_F11,    KC_F12,  _______, _______, _______, _______, _______, KC_PIPE,
-                               _______, _______, _______, KC_INS,    _______, _______, _______, _______
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  ,                     KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
+    _______, _______, _______, _______, _______, _______,                     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_EQL , QUOTE  ,
+    _______, _______, _______,TXT_COPY,TXT_PSTE, _______,S(KC_LBRC), S(KC_RBRC), _______, _______, _______, _______, _______, KC_PIPE,
+                               _______, _______, _______, _______,    KC_DEL , _______, _______, _______
   ),
 
   [_RAISE] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10, KC_PPLS,
-    _______, _______, _______, EURO   , _______, _______,                     _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,                     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT, _______, DBQUOTE,
-    _______, _______, _______, CEDILLE, _______, _______, KC_F11,    KC_F12,  _______, _______, _______, _______, _______, KC_BSLS,
-                               _______, _______, _______, KC_INS,    _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  ,                     KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
+    _______, _______, _______, _______, _______, _______,                     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT, KC_PPLS, DBQUOTE,
+    _______, _______, _______, _______, _______, _______, S(KC_9),    S(KC_0), _______, _______, _______, _______, _______, KC_BSLS,
+                               _______, _______, _______, _______,    _______, _______, _______, _______
   ),
+
 
 };
